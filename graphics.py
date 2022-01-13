9ba3290f936c8fc11a1d3be39038bbe79d2dff87
chizo <filip.cierkosz@gmail.com>
2022-01-13 13:49:43 +0100

    The graphic adjustments for the main file have been added. All of the files associated with the project have been added now.

---
+++ graphics.py
@@ -0,0 +1,43 @@
+# Color Adjustments for 2048 Game Project
+# Written by: Filip J. Cierkosz
+# Date: 11/2021
+
+# Background color for the grid.
+GRID_COLOR = '#080942'
+
+# Font style.
+FONT_BOARD = ('Comic Sans MS', 'bold')
+
+# Font colors.
+GRID_FONT_COLOR = '#080942'
+WINDOW_FONT_COLOR = '#fafafa'
+USER_FONT_COLOR = '#a2ff29'
+
+# Dictionary of colors of cells corresponding with their 
+# numerical values on the board. 
+CELL_COLORS = {
+    0:'#fafafa',
+    1:'#c4beb5',
+    2:'#a89372',
+    4:'#c28d3a',
+    8:'#c2821d',
+    16:'#d4860d',
+    32:'#e68d05',
+    64:'#f04826',
+    128:'#ed3611',
+    256:'#ff2b00',
+    512:'#e6ae2c',
+    1024:'#f0b426',
+    2048:'#ffb300',
+}
+
+# Dictionary of font sizes for different sizes of grid.
+FONT_SIZES = {
+    '3':40,
+    '4':35,
+    '5':30,
+    '6':25,
+    'finalMsg':30,
+    'score':25,
+    'select':25
+}
