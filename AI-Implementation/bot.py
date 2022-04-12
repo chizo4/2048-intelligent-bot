cb52645c895c358cbbfd92826148e067f86494a5
chizo <filip.cierkosz@gmail.com>
2022-04-12 12:20:20 +0200

    Updated the bot, so that it can win games on 5x5 grid.

--- AI-Implementation/bot.py
+++ AI-Implementation/bot.py
@@ -1,41 +1,27 @@
 '''
-2048 GAME PROJECT (AI): Artificial Intelligence Bot.
+2048 GAME PROJECT: AI Bot.
 
 Date created:
     03/2022
 
+Date edited:
+    04/2022
+
 Author:
     Filip J. Cierkosz
 '''
-
-#from gameBoard import Game2048
-#import numpy as np
-
-# Testing: Make the bot invoke 10 random moves on the board.
-'''def botRandomMoves():
-    # Possible moves on th grid.
-    moves = ['right', 'left', 'up', 'down']
-
-    # Instantiate the game.
-    botGame = Game2048()
-
-    botGame.play()
-
-    #np.random.choice(moves, 1)
-
-print(botRandomMoves())'''
 
 import random
 import numpy as np
 import pygame
 from pygame.locals import *
 from time import sleep, time
-from graphics import GRID_COLOR, CELL_COLORS, GRID_FONT_COLOR, FONT_BOARD, FONT_SIZES, USER_FONT_COLOR, WINDOW_FONT_COLOR
+from graphics import GRID_COLOR, CELL_COLORS, GRID_FONT_COLOR, FONT_BOARD, FONT_SIZES, WINDOW_FONT_COLOR
 
 class GameBot:
     '''
     -----------
-    Class to create a game board for 2048 (AI). 
+    Class to create a game board for 2048 to be solved by an AI bot. 
     -----------
     '''
     def __init__(self):
@@ -44,15 +30,15 @@ class GameBot:
 
             Parameters:
                 self
-                gs (int) : Grid size of the game board.
         '''
-        # Possible moves on the grid.
+        # Permitted moves on the grid.
         self.moves = ['right', 'left', 'up', 'down']
-        # Set the grid size to 4 by default.
-        self.GRID_SIZE = 4
+        # Default grid size.
+        self.GRID_SIZE = 5
+        self.grid = np.zeros((self.GRID_SIZE, self.GRID_SIZE), dtype=int)
         self.score = 0
         self.timer = 0
-        self.grid = np.zeros((self.GRID_SIZE, self.GRID_SIZE), dtype=int)
+        # GUI adjustments.
         self.HEIGHT = 540
         self.WIDTH = 500
         # Space at the top of GUI that is left to display time and score.
@@ -60,7 +46,6 @@ class GameBot:
         # Space between squares in the grid.
         self.SPACE = 5
         self.SQUARE_SIZE = (self.WIDTH-(self.GRID_SIZE+1)*self.SPACE)/self.GRID_SIZE
-        # Initialize pygame.
         pygame.init()
         pygame.display.set_caption("2048: GAME")
         self.window = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
@@ -134,7 +119,7 @@ class GameBot:
                 num = self.grid[r][c]
                 
                 # If a number on the grid is greater or equal to 2048, it will not
-                # change anymore, since dictionary has colors up to tha value of 2048.
+                # change anymore, since dictionary has colors up to the value of 2048.
                 if (num>=2048):
                     color = CELL_COLORS[2048]
                 else:
@@ -155,11 +140,11 @@ class GameBot:
         '''
         Updates a grid with a new number.
 
-        Probability rates for values: 4 (5%), 2 (20%), 1 (75%)
+        Probability rates for values: 2 (100%).
 
             Parameters:
                 self
-                n=1 (int) : Quantity of new numbers to append.
+                n (int) : Quantity of new numbers to be inserted.
         '''
         availableCoords = []
 
@@ -174,14 +159,15 @@ class GameBot:
 
     def makeMove(self, move):
         '''
-        Makes a move on the board based on the AI response.
+        Makes a move on the board based on the AI choice.
 
         If you wish to move to the left/right, look at the rows of the grid.
         If you wish to move up/down, look at the columns.
 
                 Parameters:
                     self
-                    move (str) : String describing the user's move.
+                    move (str) : String describing the user's move; either 'right',
+                                 'left', 'up', or 'down'.
         '''
         for i in range(self.GRID_SIZE):
             # Move to the LEFT. Define the row.
@@ -282,14 +268,16 @@ class GameBot:
 
     def play(self):
         '''
-        Main method to execute the game.
+        Main method to play the game.
 
             Parameters:
                 self
+
+            Returns:
+             True/False (boolean) : True if game is won, False otherwise.
         '''
         # Initialize the board with 2 starting numbers in the grid.
         self.insertNewNum(n=2)
-
         start = self.setTimer()
 
         # Play as long as the game is neither over, nor won by the AI bot.
@@ -303,7 +291,7 @@ class GameBot:
             # Update the screen.
             pygame.display.flip()
 
-            # If the AI bot reaches the goal state - 2048 - it denotes winning the game.
+            # If the AI bot reaches the goal state, i.e. score: 2048, it denotes win.
             if (self.score==2048):
                 self.window.fill((GRID_COLOR))
                 self.timer = self.stopTimer(start)
@@ -317,21 +305,20 @@ class GameBot:
                 # Update the window.
                 pygame.display.flip()
 
-                # Wait 5 seconds to display the final screen. Then, terminate the program.
-                sleep(5)
-                break
+                # Wait 1 second to display the final screen.
+                sleep(1)
+                return True
 
             # Make a copy of the old grid.
             oldGrid = self.grid.copy()
 
-            # SPECIFY A SEARCHED MOVE.
-            moveSelected = self.searchMove()
-            
+            # Determine the best AI-searched move.
+            mv = self.searchMove()
+
             # Execute the AI selected move.
-            self.makeMove(moveSelected)
-            #sleep(3.0) # disable this feature later
+            self.makeMove(mv)
             
-            # Check if it is possible to continue the game.
+            # Check if it is possible to continue the game after the move.
             if (self.checkIfOver()):
                 self.window.fill((GRID_COLOR))
                 self.timer = self.stopTimer(start)
@@ -345,97 +332,98 @@ class GameBot:
                 # Update the window.
                 pygame.display.flip()
 
-                # Delay 3 seconds to display the final screen. Then, terminate the program.
-                sleep(3)
-                break
+                # Delay 1 second to display the final screen.
+                sleep(1)
+                return False
 
             # Check if the grids are equal. If the grids differ insert a new number.
-            equal = (self.grid==oldGrid).all()
-
-            if (not equal):
+            if (not (self.grid==oldGrid).all()):
                 self.insertNewNum()
 
-    # TESTING STAGE, still in progress.
+    # STILL IN DEVELOPMENT STAGE!
     def searchMove(self):
-        searchDepth = 6
-        #scores = np.zeros(4)
-        scores = {mv: 0 for mv in self.moves}
-        currGrid = self.grid.copy()
+        '''
+        AI bot searches the best move performing each of the available moves, then
+        simulating future states of the game board. Finally, the best move is selected
+        analyzing the costs (the higher the cost, the better the choice). The costs
+        consist of such aspects as scores on the grid and empty spots.
 
-        # test each available move
-        # for each move create sample random search 
+            Parameters:
+                self
 
+            Returns:
+                bestMv (st) : Selected best move for the current state of the
+                              grid; either 'right', 'left', 'up', or 'down'.
+        '''
+        # Make a copy of the initial state of the grid.
+        origGrid = self.grid.copy()
+
+        # Dictionary to calculate costs per each move, based on scores and empty spots.
+        costs = {mv:0 for mv in self.moves}
+
+        # Determine the number of searches per one move and the depth of the search.
+        searchesMv = 100
+        depth = 10
+
+        # Test each available move.
         for mv in self.moves:
-            # make the move
+            # Execute the move, check if possible to continue and get the score.
             self.makeMove(mv)
-            print(f'checking move {mv}')
+            gameOverMv = self.checkIfOver()
+            scoreMv = np.max(self.grid)
 
-            if (not self.checkIfOver()):
-                # append new num
+            # If the game is not over and the move changes the state of the board,
+            # insert new number, update scores, and perform the further search.
+            if (not gameOverMv and not (self.grid==origGrid).all()):
                 self.insertNewNum()
-                
-                # perform random moves
-                for i in range(searchDepth):
-                    # select random move
-                    mvRandom = self.randomMove()
-
-                    # perform random move
-                    self.makeMove(mvRandom)
-
-                    # check if over
-                    if (not self.checkIfOver()):
-                        self.insertNewNum()
-                    # if the game is over, stop the loop
-                    else: 
-                        break
-
-
-            # equal = (self.grid==oldGrid).all()
-
-            if ((self.grid==currGrid).all()):
-                scores[mv] = 0
+                costs[mv] += scoreMv
+                counterMv = 1
+            # Otherwise, restart the grid to the initial state and move to the next move.
             else:
-                # update the dict with the maximum possible score to be obtained
-                scores[mv] = np.max(self.grid)
-                
+                self.grid = origGrid.copy()
+                continue
 
-            # reset the grid for the next move
-            self.grid = currGrid.copy()
+            # Perform simulation of later moves.
+            for i in range(searchesMv):
+                searchBoard = self.grid.copy()
+                gameOver = False
 
+                while (not gameOver and counterMv<depth):
+                    searchBoard = self.grid.copy()
+                    # Execute a random move.
+                    self.makeMove(self.shuffleMove())
+                    gameOver = self.checkIfOver()
+                    simulatedScore = np.max(self.grid)
+
+                    if (not gameOver and not (self.grid==searchBoard).all()):
+                        self.insertNewNum()
+                        costs[mv] += simulatedScore
+                        counterMv += 1
+
+                # Increment the costs by the number of empty spots in the current state of grid.
+                costs[mv] += 10*np.count_nonzero(self.grid==0)
+            
+            # Restart the grid to the initial state for the next move to be tested.
+            self.grid = origGrid.copy()
+
+        # Finally, restart the grid to the initial state one last time.
+        self.grid = origGrid.copy()
+
+        # Find and return the best searched move.
+        print(costs)
+        bestMv = max(costs, key=costs.get)
+        return bestMv
         
-        boolX = True
-        #if (not boolX):
-        #if (len(set(scores.values()))==1):
-        if (all(val == 0 for val in scores.values())):
-            mvBest = random.choice(list(scores.values()))
-        else: 
-            # select the best move, key with max value
-            mvBest = max(scores, key=scores.get)
+    def shuffleMove(self):
+        '''
+        Shuffles a random move.
 
-        # reset the grid to the starting state
-        self.grid = currGrid
-        # return the best move to select
-        print(mvBest)
-        return mvBest
+            Parameters:
+                self
 
-
-
-
-
-                    
-
-    def randomMove(self):
-        moves = ['right', 'left', 'up', 'down']
-        choice = np.random.choice(moves, 1)
-        #self.makeMove(choice)
-
-        return choice
-
-
-
-
-
-
-# Testing...
-bot = GameBot()
-bot.play()
+            Returns:
+                randMv : One randomly selected move; either 'right', 'left',
+                         'up', or 'down'.
+        '''
+        randMv = np.random.choice(self.moves, 1)
+        return randMv
