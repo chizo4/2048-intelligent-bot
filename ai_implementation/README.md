59ce8c54084310139b90962fb0747165964f5b37
chizo <filip.cierkosz@gmail.com>
2022-09-30 10:29:39 +0100

    Code base cleanup for ai_implementation and notebooks updates.

--- ai_implementation/README.md
+++ ai_implementation/README.md
@@ -20,8 +20,8 @@ Rules:
 The AI was tested for slightly more than 500 samples and the results were stored in a database. 
 
 Performance highlights based on [data](https://github.com/chizo4/2048-Project/blob/main/ai_implementation/notebook/bot_notebook.ipynb) collected while testing:
-- ```Probability of AI winning a game : 30.2%```
-- ```Probability of a score greater or equal to 1024 : 88.1%```
+- ```Probability of AI winning a game : 30.4%```
+- ```Probability of a score greater or equal to 1024 : 88.0%```
 - ```Average time to win a game : 68.4 sec```
 
 ❗ NB: The average time records can be rather described as biased, since it is mainly dependant on the performance of your machine. The reference point for time measurements in this experiment was 2020 M1 MacBook Pro.
--- ai_implementation/bot/bot.py
+++ ai_implementation/bot/bot.py
@@ -1,17 +1,11 @@
 '''
-bot.py
+ai_implementation/bot/bot.py
 
-2048 GAME PROJECT: AI Bot implementation (more info in README.md).
+2048-Project: AI bot implementation (more info in README.md).
 
-Date created:
-    03/2022
-
-Date edited:
-    07/2022
-
-Author:
-    Filip J. Cierkosz
+Author: Filip J. Cierkosz (2022)
 '''
+
 
 import random
 import numpy as np
@@ -20,12 +14,14 @@ from pygame.locals import *
 from time import sleep, time
 from bot.graphics_bot import *
 
+
 class GameBot:
     '''
     -----------
     Class to create a game board for 2048 to be solved by an AI bot. 
     -----------
     '''
+
     def __init__(self):
         '''
         Constructor to initialize an appropriately-sized grid for the game with all attributes.
@@ -44,11 +40,11 @@ class GameBot:
         self.grid = np.zeros((self.GRID_SIZE, self.GRID_SIZE), dtype=int)
         self.HEIGHT = 540
         self.WIDTH = 500
-        self.TOP_SPACE = self.HEIGHT-self.WIDTH
+        self.TOP_SPACE = self.HEIGHT - self.WIDTH
         self.SPACE = 5
-        self.SQUARE_SIZE = (self.WIDTH-(self.GRID_SIZE+1)*self.SPACE)/self.GRID_SIZE
+        self.SQUARE_SIZE = (self.WIDTH - (self.GRID_SIZE + 1) * self.SPACE) / self.GRID_SIZE
         pygame.init()
-        pygame.display.set_caption("2048: GAME")
+        pygame.display.set_caption('2048: AI BOT')
         self.window = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
         pygame.font.init()
         self.font_game = pygame.font.SysFont(
@@ -88,23 +84,23 @@ class GameBot:
             Returns:
                 new (np.array) : Updated column/row to the grid.
         '''
-        temp = [n for n in curr if (n!=0)]
+        temp = [n for n in curr if (n != 0)]
         new = []
         skip = False
 
         for i in range(len(temp)):
             # Skip an element that was just added (so that it is not repeated).
-            if (skip):
+            if skip:
                 skip = False
                 continue
             # If two consecutive elements are equal, add them and append in new.
-            if (i!=len(temp)-1 and temp[i]==temp[i+1]):
+            if (i != len(temp) - 1) and (temp[i] == temp[i + 1]):
                 skip = True
-                new.append(2*temp[i])
+                new.append(2 * temp[i])
             else:
                 new.append(temp[i])
 
-        while (len(new)!=len(curr)):
+        while len(new) != len(curr):
             new.append(0)
 
         return np.array(new)
@@ -121,13 +117,13 @@ class GameBot:
         # Display squares in the NxN grid.
         for r in range(self.GRID_SIZE):
             for c in range(self.GRID_SIZE):
-                x = (c+1)*self.SPACE+c*self.SQUARE_SIZE
-                y = self.TOP_SPACE+(r+1)*self.SPACE+r*self.SQUARE_SIZE
+                x = (c + 1) * self.SPACE + c * self.SQUARE_SIZE
+                y = self.TOP_SPACE + (r + 1) * self.SPACE + r * self.SQUARE_SIZE
                 num = self.grid[r][c]
                 
                 # If a number on the grid is greater or equal to 2048, it will not
                 # change anymore, since dictionary has colors up to 2048.
-                if (num>=2048):
+                if num >= 2048:
                     color = CELL_COLORS[2048]
                 else:
                     color = CELL_COLORS[num]
@@ -135,11 +131,11 @@ class GameBot:
                 pygame.draw.rect(
                     self.window,
                     color,
-                    pygame.Rect(x,y,self.SQUARE_SIZE,self.SQUARE_SIZE),
+                    pygame.Rect(x, y, self.SQUARE_SIZE, self.SQUARE_SIZE),
                     border_radius=8
                 )
 
-                if (num!=0):
+                if num != 0:
                     text_area = self.font_game.render(
                         f'{num}',
                         True,
@@ -147,8 +143,9 @@ class GameBot:
                     )
                     self.window.blit(
                         text_area,
-                        text_area.get_rect(center=(x+self.SQUARE_SIZE/2,
-                        y+self.SQUARE_SIZE/2))
+                        text_area.get_rect(
+                            center=(x + self.SQUARE_SIZE / 2, y + self.SQUARE_SIZE / 2)
+                        )
                     )
 
     def insert_new_num(self, n=1):
@@ -164,7 +161,7 @@ class GameBot:
         available_coords = []
 
         for row, col in np.ndindex(self.grid.shape):
-            if (self.grid[row][col]==0):
+            if self.grid[row][col] == 0:
                 available_coords.append((row, col))
 
         for c in random.sample(available_coords, k=n):
@@ -183,24 +180,24 @@ class GameBot:
                                 'left', 'up', or 'down'.
         '''
         for i in range(self.GRID_SIZE):
-            if (move=='left'):
+            if move == 'left':
                 curr = self.grid[i]
-            elif (move=='right'):
+            elif move == 'right':
                 curr = self.grid[i][::-1]
-            elif (move=='up'):
+            elif move == 'up':
                 curr = self.grid[:, i]
-            elif (move=='down'):
+            elif move == 'down':
                 curr = self.grid[:, i][::-1]
 
             new = self.update_arr(curr)
 
-            if (move=='left'):
+            if move == 'left':
                 self.grid[i] = new
-            elif (move=='up'):
+            elif move == 'up':
                 self.grid[:, i] = new
-            elif (move=='right'):
+            elif move == 'right':
                 self.grid[i] = new[::-1]
-            elif (move=='down'):
+            elif move == 'down':
                 self.grid[:, i] = new[::-1]
 
     def check_if_over(self):
@@ -218,7 +215,7 @@ class GameBot:
         for mv in self.moves:
             self.make_move(mv)
 
-            if (not (self.grid==original).all()):
+            if not (self.grid == original).all():
                 self.grid = original
                 return False
 
@@ -238,14 +235,14 @@ class GameBot:
                 best_mv (str) : best searched move ('right'/'left'/'up'/'down').
         '''
         original_grid = self.grid.copy()
-        costs =  {mv:0 for mv in self.moves}
+        costs =  {mv: 0 for mv in self.moves}
 
         for first_mv in self.moves:
             self.make_move(first_mv)
             game_over_first_mv = self.check_if_over()
             score_first_mv = np.max(self.grid)
 
-            if (not game_over_first_mv and not (self.grid==original_grid).all()):
+            if (not game_over_first_mv) and (not (self.grid == original_grid).all()):
                 self.insert_new_num()
                 costs[first_mv] += score_first_mv
                 search_board_after_first_insert = self.grid.copy()
@@ -257,27 +254,26 @@ class GameBot:
                 self.grid = search_board_after_first_insert.copy()
                 game_over = False
 
-                while (counter<self.SEARCH_DEPTH and not game_over):
+                while counter < self.SEARCH_DEPTH and not game_over:
                     random_mv = self.shuffle_move()
                     prev_simulated_grid = self.grid.copy()
                     self.make_move(random_mv)
                     new_simulated_score = self.get_score()
                     game_over = self.check_if_over()
 
-                    if (not game_over and not (self.grid==prev_simulated_grid).all()):
+                    if not game_over and not (self.grid == prev_simulated_grid).all():
                         self.insert_new_num()
                         costs[first_mv] += new_simulated_score
-                        counter += 1  
-                costs[first_mv] += self.EMPTY_SPOT_CONST*np.count_nonzero(self.grid==0)
+                        counter += 1
+                costs[first_mv] += self.EMPTY_SPOT_CONST * np.count_nonzero(self.grid == 0)
             self.grid = original_grid.copy()
 
         # Find the best move (one with the highest costs).
         best_mv = max(costs, key=costs.get)
-
         # Reset the grid to its original state.
         self.grid = original_grid.copy()
 
-        if (all(val==0 for val in costs.values())):
+        if all(val == 0 for val in costs.values()):
             return self.shuffle_move()
         else:
             return best_mv
@@ -289,7 +285,7 @@ class GameBot:
     set_timer = lambda self: time()
 
     # Stops the timer and returns the time of execution.
-    stop_timer = lambda self, start: time()-start
+    stop_timer = lambda self, start: time() - start
 
     # Returns boolean denoting if bot won the game.
     is_win = lambda self: self.win
@@ -322,12 +318,12 @@ class GameBot:
             )
             self.window.blit(
                 text_area,
-                text_area.get_rect(center=(115,20))
+                text_area.get_rect(center=(115, 20))
             )
             pygame.display.flip()
 
             # Case: BOT WIN.
-            if (self.score==2048):
+            if (self.score == 2048):
                 self.window.fill((GRID_COLOR))
                 self.timer = self.stop_timer(start)
                 text_area = self.font_msg.render(
@@ -337,7 +333,9 @@ class GameBot:
                 )
                 self.window.blit(
                     text_area,
-                    text_area.get_rect(center=(self.WIDTH/2,self.HEIGHT/2-50))
+                    text_area.get_rect(
+                        center=(self.WIDTH / 2, self.HEIGHT / 2 - 50)
+                    )
                 )
                 text_area = self.font_msg.render(
                     f'TIME PLAYED: {self.timer:.1f} SEC',
@@ -346,7 +344,9 @@ class GameBot:
                 )
                 self.window.blit(
                     text_area,
-                    text_area.get_rect(center=(self.WIDTH/2,self.HEIGHT/2+20))
+                    text_area.get_rect(
+                        center=(self.WIDTH / 2, self.HEIGHT / 2 + 20)
+                    )
                 )
                 pygame.display.flip()
                 self.win = 1
@@ -358,7 +358,7 @@ class GameBot:
             self.make_move(next_move)
             
             # Case: BOT LOSS.
-            if (self.check_if_over()):
+            if self.check_if_over():
                 self.window.fill((GRID_COLOR))
                 self.timer = self.stop_timer(start)
                 text_area = self.font_msg.render(
@@ -368,7 +368,9 @@ class GameBot:
                 )
                 self.window.blit(
                     text_area,
-                    text_area.get_rect(center=(self.WIDTH/2,self.HEIGHT/2-50))
+                    text_area.get_rect(
+                        center=(self.WIDTH / 2, self.HEIGHT / 2 - 50)
+                    )
                 )
                 text_area = self.font_msg.render(
                     f'TIME PLAYED: {self.timer:.1f} SEC',
@@ -377,11 +379,13 @@ class GameBot:
                 )
                 self.window.blit(
                     text_area,
-                    text_area.get_rect(center=(self.WIDTH/2,self.HEIGHT/2+20))
+                    text_area.get_rect(
+                        center=(self.WIDTH / 2, self.HEIGHT / 2 + 20)
+                    )
                 )
                 pygame.display.flip()
                 sleep(1)
                 return False
 
-            if (not (self.grid==old_grid).all()):
+            if (not (self.grid == old_grid).all()):
                 self.insert_new_num()
